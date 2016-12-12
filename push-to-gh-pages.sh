62fc4cafcafa6a0456f3e109b54ca2c802b2018e
Herman Starikov <herman.starikov@uxpsystems.com>
2016-12-11 20:15:35 -0500

    add publishing script

---
+++ push-to-gh-pages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git branch -D gh-pages
+git push origin :gh-pages
+git checkout -b gh-pages
+polymer build
+cp -r build/unbundled/* .
+git add .
+git commit -m 'update gh-pages'
+git push origin gh-pages
+git checkout master
