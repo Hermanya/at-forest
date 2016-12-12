e9cd8cc2c48e1728830627c0a1eda92d2c7d3b4d
Herman Starikov <herman.starikov@uxpsystems.com>
2016-12-11 21:04:01 -0500

    unprod build

--- push-to-gh-pages.sh
+++ push-to-gh-pages.sh
@@ -2,8 +2,8 @@
 git branch -D gh-pages
 git push origin :gh-pages
 git checkout -b gh-pages
-polymer build
-cp -r build/unbundled/* .
+#polymer build
+#cp -r build/unbundled/* .
 rm .gitignore
 git add .
 git commit -m 'update gh-pages'
